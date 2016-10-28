f0bdff2ef1c1cd302d9350df03ebc77fcc5cd65e
istama <night.watch.kc@gmail.com>
2016-10-28 21:48:15 +0900

    update AddressItem: add comment

--- src/AddressItem.vb
+++ src/AddressItem.vb
@@ -1,10 +1,14 @@
 ﻿'
 ' 日付: 2016/10/10
 '
+
+''' <summary>
+''' 住所の項目
+''' </summary>
 Public Enum AddressItem
-  Zipcode
-  Prefecture
-  City
-  TownArea
-  Office
+  Zipcode     ' 郵便番号
+  Prefecture  ' 都道府県
+  City        ' 市町村
+  TownArea    ' 町域
+  Office      ' 事業所、郵便局
 End Enum
