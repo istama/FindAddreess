015708d146769285a2c619c9f60745584bb02b1e
istama <night.watch.kc@gmail.com>
2016-10-31 11:33:14 +0900

    update TestAddressWords: add test of whether it adaptive the Office csv text

--- test/TestAddressWords.vb
+++ test/TestAddressWords.vb
@@ -47,6 +47,15 @@ Public Class TestAddressWords
     Assert.AreEqual("新宿区",  addr1.City(0))
     Assert.AreEqual("豊島区",  addr1.City(1))
     Assert.AreEqual("西新宿",  addr1.TownArea(0))    
+    
+    Dim addr2 As AddressWords = AddressWords.CreateFromCsvOfAddressTextAndOffice("1000001,東京都千代田区,丸の内オフィス")
+    Assert.AreEqual("1000001",       addr2.Zipcode(0))
+    Assert.AreEqual("東京都千代田区", addr2.Prefecture(0))
+    Assert.AreEqual("丸の内オフィス",    addr2.Office(0))
+    
+    Dim addr3 As AddressWords = AddressWords.CreateFromCsvOfZipAndOffice("1600001,新宿ビル")
+    Assert.AreEqual("1600001",       addr3.Zipcode(0))
+    Assert.AreEqual("新宿ビル",    addr3.Office(0))
   End Sub
   
   <Test> _
